27a66213a7c6446605e46de6aa56f723cfe45f1f
jcalahi <julius_calahi@optum.com>
2016-04-22 22:00:16 +0800

    * updates

--- app.js
+++ app.js
@@ -3,37 +3,6 @@ var express = require('express'),
     app = express(), // jshint ignore:line
     PORT = process.env.PORT || 8080; // jshint ignore:line
 
-/*client.index({
-    index: 'pacs',
-    type: 'dicoms',
-    id: '6',
-    body: {
-        "InstanceCreationDate": "20050726",
-        "InstanceCreationTime": "103902",
-        "StudyTime": "010100.000000",
-        "AccessionNumber": "1",
-        "Modality": "CT",
-        "InstitutionName": "National Library of Medicine",
-        "InstitutionAddress": "http://www-creatis.insa-lyon.fr/Public/Gdcm",
-        "ReferringPhysicianName": "Unknown",
-        "StudyDescription": "Visible Human Female",
-        "SeriesDescription": "Resampled to 1mm voxels",
-        "PatientName": "Jhing",
-        "PatientID": "000-000-001",
-        "PatientBirthDate": "20100101",
-        "PatientBirthTime": "010100.000000",
-        "PatientSex": "F",
-        "SliceThickness": "1.0",
-        "PatientPosition": "HFS",
-        "StudyID": "1",
-        "SeriesNumber": "1",
-        "InstanceNumber": "66",
-        "HDFSfilePath": "http://localhost:8080/dicomviewer/public/images/vhm.1035.dcm"
-    }
-}, function(error, response) {
-    console.log(response);
-});*/
-
 app.use(express.static(path.join(__dirname + '/public')));
 app.use('/dicomviewer', express.static(path.join(__dirname + '/views')));
 
